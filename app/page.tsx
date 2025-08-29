75e541cbdf72d4c16da0b976a718d65c4cffa460
Shoujanya <jptmax123@gmail.com>
2025-08-29 17:38:40 +0545

    02-database

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  // const user = db.user
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <Button>Get Started</Button>
